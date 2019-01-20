0803034103ff88b04b1467552fc085536ac07c99
fallen potato <cruzjon32@gmail.com>
2019-01-19 21:29:35 -0700

    updated a bit of things

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,86 @@
 var friends = [
 
+    {
+    name: 'Turtle',
+    photo: 'https://www.google.com/search?q=random+jpg+images&safe=off&tbm=isch&source=iu&ictx=1&fir=oyN-199AOjE5hM%253A%252CSQkNlu3t8btZyM%252C_&usg=AI4_-kRuYtuIAwB4Q7gzeZCDdwLldgahuQ&sa=X&ved=2ahUKEwjCm8yrwPvfAhWFK3wKHRH8BjQQ9QEwA3oECAIQCg#imgrc=oyN-199AOjE5hM:',
+    scores: [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5'
+    ]
+},
+{
+    name: 'Laptop',
+    photo: 'https://picsum.photos/200/300?image=0',
+    scores: [
+        '4',
+        '5',
+        '2',
+        '2',
+        '1',
+        '2',
+        '1',
+        '5',
+        '5',
+        '3'
+    ]
+},
+{
+    name: 'Blurred Laptop',
+    photo: 'https://picsum.photos/200/300/?blur',
+    scores: [
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1'
+    ]
+},
+{
+    name: 'random',
+    photo: 'https://picsum.photos/200/300/?image=184',
+    scores: [
+        '2',
+        '2',
+        '2',
+        '2',
+        '2',
+        '2',
+        '2',
+        '2',
+        '2',
+        '2'
+    ]
+},
+{
+    name: 'more random',
+    photo: 'https://picsum.photos/200/300/?image=184',
+    scores: [
+        '3',
+        '3',
+        '3',
+        '3',
+        '3',
+        '3',
+        '3',
+        '3',
+        '3',
+        '3'
+    ]
+},
+
 ]
 
 
