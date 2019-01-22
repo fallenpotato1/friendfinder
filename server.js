869c51f9066705bbb4f41b52432e29d973c51ddb
fallen potato <cruzjon32@gmail.com>
2019-01-21 21:40:23 -0700

    added the jawsDB thing

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var mysql = require("mysql")
+var connection
 
 var app = express();
 var PORT = process.env.PORT || 8080;
-
-app.use(express.static(path.join(__dirname, './app/public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,3 +17,17 @@ require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 app.listen(PORT, function() {
   console.log('Friend Finder app is listening on PORT: ' + PORT);
 });
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(preocess.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "fallenpotato",
+        database: "todoagain+_db"
+    })
+}
+
+connection.connect()
+module.exports = connection
